50e1239a0f7516d8578c685c5159af764026d85b
andyk2000 <125449101+andyk2000@users.noreply.github.com>
2024-10-31 19:39:21 +0200

    feat: creating the model

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,42 @@
-import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
-var app = express();
-app.use(express.static("public"));
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import express, { Express } from "express";
+import { Sequelize } from "sequelize";
+import bodyParser from "body-parser";
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+interface Config {
+  host: string;
+  user: string;
+  password: string;
+  database: string;
+  frontend: string;
+}
+
+const config: Config = {
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_DATABASE || "mydatabase",
+  frontend: process.env.FRONTEND_LINK || "urubuto",
+};
+
+console.log(config);
+
+const sequelize = new Sequelize(config.database, config.user, config.password, {
+  host: config.host,
+  dialect: "postgres",
+  logging: false,
 });
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+app.use(express.static("public"));
+
+app.listen(port, async () => {
+  await sequelize.sync({ alter: true });
+  console.log("Server Listening on PORT:", port);
 });
