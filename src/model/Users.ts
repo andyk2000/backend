c68581b8659936ee9020e18c935812a2c139b4f3
andyk2000 <125449101+andyk2000@users.noreply.github.com>
2024-11-03 22:58:41 +0200

    fix: fixed the naming issues

---
+++ src/model/Users.ts
@@ -0,0 +1,73 @@
+import {
+  DataTypes,
+  Sequelize,
+  Model,
+  Op,
+  InferCreationAttributes,
+  InferAttributes,
+  CreationOptional,
+  CreationAttributes,
+} from "sequelize";
+
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+  declare id: CreationOptional<number>;
+  declare names: string;
+  declare email: string;
+  declare password: string;
+  declare typeOfAccount: string;
+  declare institutionId: number;
+  declare title: string;
+  declare phone: number;
+}
+
+const initializeUser = (sequelize: Sequelize) => {
+  User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        names: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        typeOfAccount: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: "customer",
+        },
+        institutionId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: "doctor"
+        },
+        phone: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        }
+    },
+    {
+      sequelize,
+      modelName: "user",
+      timestamps: false,
+    },
+  );
+};
+
+export {
+  initializeUser,
+};
